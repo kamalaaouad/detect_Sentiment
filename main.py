f45da1b93b8311fc45e8c7924c4ff178230b71d0
Utilisateur <kammalaaouad1999@gmail.com>
2022-06-03 01:55:29 +0100

    first release

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+
+import uvicorn
+from fastapi import FastAPI
+from transformers import pipeline
+
+path = './distilbert-base-uncased-finetuned-sst-2-english'
+
+sentiment_analysis = pipeline("sentiment-analysis", model=path)
+
+"""sentiment_analysis = pipeline(
+    "sentiment-analysis",
+    model="avichr/heBERT_sentiment_analysis",
+    tokenizer="avichr/heBERT_sentiment_analysis",
+    return_all_scores = False
+)
+"""
+app = FastAPI()
+
+
+@app.post("/detect_sentiment")
+async def detect_sentiment(sentence: str):
+    res = sentiment_analysis(sentence)
+    return json.dumps(res[0])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080,reload=True)
